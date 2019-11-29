5086ce7e5ea0dea477ea155c0b07c82a69da2834
EkaterinaBelonosova <33663086+EkaterinaBelonosova@users.noreply.github.com>
2019-11-29 12:44:47 +0700

    Add files via upload

---
+++ build/precache-manifest.8b4bec540ff9f5a98f9afa38cfa37faa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f4b0c9df1169535f4c23bd86e22c20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92aaa2cf8f41490bacaf",
+    "url": "/static/css/2.dc13051e.chunk.css"
+  },
+  {
+    "revision": "846f1dfa05be59e37a1a",
+    "url": "/static/css/main.0a9878c2.chunk.css"
+  },
+  {
+    "revision": "92aaa2cf8f41490bacaf",
+    "url": "/static/js/2.1e24a07a.chunk.js"
+  },
+  {
+    "revision": "846f1dfa05be59e37a1a",
+    "url": "/static/js/main.89f10f7d.chunk.js"
+  },
+  {
+    "revision": "71c00c39f8bd4e1128c1",
+    "url": "/static/js/runtime-main.8dba2ea7.js"
+  }
+]);
