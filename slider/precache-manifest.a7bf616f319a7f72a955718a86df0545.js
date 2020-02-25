5a5ce145893bf3416affdc526045dc930640f83a
Ekaterina Dyukova <C:\Users\e.dyukova\AppData\Roaming\The Bat!\Mail>
2020-02-25 09:38:45 +0700

    slider

---
+++ slider/precache-manifest.a7bf616f319a7f72a955718a86df0545.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f5ae17a07d33dbbfaa40aefc75d110",
+    "url": "./index.html"
+  },
+  {
+    "revision": "35071ca3cdac71198c8a",
+    "url": "./static/css/2.4e47561d.chunk.css"
+  },
+  {
+    "revision": "fce51f13e09eed20299b",
+    "url": "./static/css/main.8a356037.chunk.css"
+  },
+  {
+    "revision": "35071ca3cdac71198c8a",
+    "url": "./static/js/2.f95bd699.chunk.js"
+  },
+  {
+    "revision": "cfa9e58c4061ecd3e3f1bea80f45dc8d",
+    "url": "./static/js/2.f95bd699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce51f13e09eed20299b",
+    "url": "./static/js/main.86ce441a.chunk.js"
+  },
+  {
+    "revision": "567ca5cea940a0c54188",
+    "url": "./static/js/runtime-main.1a632c21.js"
+  }
+]);
